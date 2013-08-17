58665cca3237c3a3c00801a0215a2b15dac7a637
Alexander Wenzel <elkangaroo@googlemail.com>
2013-08-17 18:44:37 +0200

    add 2 minutes timer and simple game over screen

--- main.lua
+++ main.lua
@@ -2,41 +2,54 @@ require 'building'
 require 'player'
 require 'background'
 
-local app = {
+local states = { start = 0, play = 1, over = 2 }
+
+local game = {
 	version = 0.1,
-	showdebug = false,
-	showhelp = false
+	state = states.start
 }
 
 local building = Building(10)
 local player = Player()
 local background = Background()
 
+local remainingTime = 120
+
 local offsetX = 90
 local offsetY = 100
+
 local scale = 2
 
 love.graphics.setDefaultImageFilter('linear', 'nearest')
 
 function love.load(...)
+	game.state = states.play
+
 	building:load(...)
 	background:load(...)
-  player:load()
+  player:load(...)
 	player:setLimit(building:getLimit())
 end
 
 function love.update(dt)
 	background:update(st)
 	building:update(dt)
-  player:update(dt)
+	player:update(dt)
+
+	if remainingTime - dt >= 0 then
+		remainingTime = remainingTime - dt
+	else
+		game.state = states.over
+	end
 end
 
 function love.draw()
-  love.graphics.scale(scale, scale)
-
-	background:draw(offsetX, offsetY)
-	building:draw(offsetX, offsetY)
-  player:draw(offsetX, offsetY)
+	if states.play == game.state then
+		game.drawStatePlay()
+		game.drawGui()
+	elseif states.over == game.state then
+		game.drawStateOver()
+	end
 end
 
 function love.focus(focused)
@@ -48,13 +61,11 @@ function love.quit()
 end
 
 function love.keypressed(key, unicode)
+	if 'up' == key and building:canMoveUpAt(player.position.x, player.position.y) then
+		building:moveUp()
+	end
 	if 'escape' == key then
 		love.event.push('quit')
-	end
-	if 'up' == key then
-		if building:canMoveUpAt(player.position.x, player.position.y) then
-			building:moveUp()
-		end
 	end
 end
 
@@ -76,5 +87,33 @@ end
 
 function love.joystickreleased(joystick, button)
 
+end
+
+function game.drawStateStart()
+
+end
+
+function game.drawStatePlay()
+	love.graphics.scale(scale, scale)
+
+	background:draw(offsetX, offsetY)
+	building:draw(offsetX, offsetY)
+	player:draw(offsetX, offsetY)
+end
+
+function game.drawStateOver()
+	love.graphics.clear()
+	love.graphics.push()
+		love.graphics.setColor(85, 190, 240)
+		love.graphics.printf('GAME OVER', 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), 'center')
+	love.graphics.pop()
+end
+
+function game.drawGui()
+	love.graphics.push()
+		love.graphics.setColor(85, 190, 240)
+		love.graphics.print('Time: ' .. string.format('%.1f', remainingTime), 2, 2)
+		love.graphics.print('FPS: ' .. love.timer.getFPS(), 2, 22)
+	love.graphics.pop()
 end
 
