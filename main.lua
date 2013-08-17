a79f8971adb182c0a6d8191b35b7a52685facdc9
Alexander Wenzel <elkangaroo@googlemail.com>
2013-08-17 13:44:07 +0200

    add empty template lua files

---
+++ main.lua
@@ -0,0 +1,44 @@
+function love.load(...)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.focus(focused)
+
+end
+
+function love.quit()
+
+end
+
+function love.keypressed(key, unicode)
+
+end
+
+function love.keyreleased(key, unicode)
+
+end
+
+function love.mousepressed(x, y, button)
+
+end
+
+function love.mousereleased(x, y, button)
+
+end
+
+function love.joystickpressed(joystick, button)
+
+end
+
+function love.joystickreleased(joystick, button)
+
+end
+
