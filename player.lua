433a19c92e836500cfa16f9096b0ad8b24a44d92
Alexander Wenzel <elkangaroo@googlemail.com>
2013-08-17 20:27:59 +0200

    smooth animation; stop animation when char stops

--- player.lua
+++ player.lua
@@ -7,18 +7,20 @@ function Player()
 		playerImages.ani_left = love.graphics.newImage('player_left.png')
 		playerImages.ani_right = love.graphics.newImage('player_right.png')
 
+		self.direction = 'right'
     self.speed = 2
     self.position = { x = 0, y = 0 }
 		self.limit = { x = 0, y = 0 }
 		self.size = { x = 24, y = 33 }
 
 		self.animation = newAnimation(playerImages.ani_right, self.size.x, self.size.y, 0.1, 0)
+		self.animation:stop()
   end
 
   function player:update(dt, freeze_controls)
-  	if not freeze_controls then
-			self.animation:update(dt)
+		self.animation:update(dt)
 
+		if not freeze_controls then
 			if love.keyboard.isDown('left') and 0 < self.position.x then
 			  self.position.x = self.position.x - self.speed
 			elseif love.keyboard.isDown('right') and (self.limit.x - self.size.x) > self.position.x then
@@ -37,15 +39,24 @@ function Player()
   end
 
   function player:keypressed(key, unicode)
-		if 'left' == key then
-			self.animation = newAnimation(playerImages.ani_left, self.size.x, self.size.y, 0.1, 0)
+		if 'left' == key or 'up' == key then
+			if 'left' ~= self.direction then
+				self.animation = newAnimation(playerImages.ani_left, self.size.x, self.size.y, 0.1, 0)
+			end
+			self.animation:play()
+			self.direction = 'left'
 		elseif 'right' == key then
-			self.animation = newAnimation(playerImages.ani_right, self.size.x, self.size.y, 0.1, 0)
+			if 'right' ~= self.direction then
+				self.animation = newAnimation(playerImages.ani_right, self.size.x, self.size.y, 0.1, 0)
+			end
+			self.animation:play()
+			self.direction = 'right'
 		end
   end
 
 	function player:keyreleased(key, unicode)
-		--self.animation:stop()
+		self.animation:stop()
+		self.animation:reset()
 	end
 
 	function player:setLimit(x, y)
